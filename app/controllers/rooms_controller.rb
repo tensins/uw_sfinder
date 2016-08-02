9512b7fec195432678f0534337aa133200412fac
Tenzin Dhundup <h20ocelot@hotmail.com>
2016-08-02 16:40:15 -0400

    ok NOW finito

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,7 @@ class RoomsController < ApplicationController
     
     @all_rooms = find_all_by("building",@building)
    	@all_rooms.each do |room|
-   		@all_rooms.delete(room) if !(room.is_vacant?(Time.new(2016,7,11,9,0,0,Time.now.utc_offset))) # delete rooms that aren't vacant
+   		@all_rooms.delete(room) if !(room.is_vacant?(Time.now)) # delete rooms that aren't vacant
     end
     @all_rooms = sort_by_time(@all_rooms) # sort the table according to start times
   	render 'index'
