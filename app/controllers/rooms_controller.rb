2423e30fd7d2259655f98ff0b3291ff25046cb9b
Tenzin Dhundup <h20ocelot@hotmail.com>
2017-06-11 01:04:37 -0400

    visual update

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,24 +4,25 @@ class RoomsController < ApplicationController
   def index
     # get the building passed in
     inp = params[:building]
-    inp_arr = inp.split(' ')
-    @all_rooms = []
-    @curr_time = Time.now()
-    @building = inp_arr[0].upcase # remove leading and trail spaces
-    if (inp_arr.length > 1)
-      @room_name = @building + " " + inp_arr[1]
-      @room = Room.find_by(room_name:@room_name)
-      @all_rooms << @room unless @room.nil?
-      @all_rooms = sort_by_time(@all_rooms)
-    else
-      # get all valid rooms in that building
-      @all_rooms = find_all_by("building",@building) # get all rooms in building
-      @all_rooms.delete_if {|room| !room.is_vacant? @curr_time}
-      @all_rooms = sort_by_time(@all_rooms) # sort the table according to start times
+    # @home will be used to determine if user just arriving at home page
+    @home = inp.nil?
+    if !@home
+      inp_arr = inp.split(' ')
+      @all_rooms = []
+      @curr_time = Time.now()
+      @building = inp_arr[0].upcase # remove leading and trail spaces
+      if (inp_arr.length > 1)
+        @room_name = @building + " " + inp_arr[1]
+        @room = Room.find_by(room_name:@room_name)
+        @all_rooms << @room unless @room.nil?
+        @all_rooms = sort_by_time(@all_rooms)
+      else
+        # get all valid rooms in that building
+        @all_rooms = find_all_by("building",@building) # get all rooms in building
+        @all_rooms.delete_if {|room| !room.is_vacant? @curr_time}
+        @all_rooms = sort_by_time(@all_rooms) # sort the table according to start times
+      end
     end
-    
   end
 
-  def home
-  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	root 'rooms#home'
-	get 'find' => 'rooms#index'
+	root 'rooms#index'
+	get '/find' => 'rooms#index'
 end 
